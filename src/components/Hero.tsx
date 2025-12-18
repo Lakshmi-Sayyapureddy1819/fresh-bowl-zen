6034c173630aa36137016adeb38e602484f806f5
Lakshmi Sayyapureddy <142775745+Lakshmi-Sayyapureddy1819@users.noreply.github.com>
2025-12-18 23:10:00 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -94,21 +94,6 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Scroll Indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.2 }}
-        className="absolute bottom-8 left-1/2 -translate-x-1/2"
-      >
-        <motion.div
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 1.5, repeat: Infinity }}
-          className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center"
-        >
-          <div className="w-1.5 h-3 bg-white/50 rounded-full mt-2" />
-        </motion.div>
-      </motion.div>
     </section>
   );
 };
